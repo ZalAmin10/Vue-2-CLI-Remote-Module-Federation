d304e8169e316869945cc6f4724e55f49b3a0d0f
Zal Amin <azmizalll123@gmail.com>
2024-02-08 12:29:47 +0800

    Vue 2 cli Remote

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const federation = require('./federation.json');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  publicPath: "http://localhost:8081",
+  chainWebpack: config => {
+    config.optimization.delete('splitChunks');
+    config
+    .plugin('module-federation-plugin')
+    .use(require('webpack').container.ModuleFederationPlugin, [{
+      ...federation,
+      shared: {
+        "vue": {
+          eager: true,
+          singleton: false
+        }
+      },
+      filename: "remoteEntry.js"
+    }])
+  },
+});
